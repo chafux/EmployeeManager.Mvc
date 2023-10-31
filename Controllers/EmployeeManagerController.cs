88a6fce3cd9be14dfa4f8fe538d52e5efb2d4784
Mohamed Amine Chafia <chafia.amine1@gmail.com>
2023-10-30 22:41:08 -0400

    Insert  a New Employee

--- Controllers/EmployeeManagerController.cs
+++ Controllers/EmployeeManagerController.cs
@@ -27,6 +27,36 @@ namespace EmployeeManager.Mvc.Controllers
         }
 
 
+        public IActionResult List() 
+        {
+            List<Employee> model = (from e in db.Employees orderby e.EmployeeID select e).ToList();
+
+            return View(model);
+        
+        }
+
+        public IActionResult Insert() 
+        {
+            FillCountries();
+            return View();
+        
+        }
+
+
+        [HttpPost]
+        public IActionResult Insert(Employee model) 
+        {
+            FillCountries();
+            if (ModelState.IsValid) 
+            {
+                db.Employees.Add(model);
+                db.SaveChanges();
+                ViewBag.Message = "Employee inserted successfully";
+            }
+            return View(model);
+        
+        }
+
 
 
     }
