1522186f673e093d919bceb7adfa0f877ea5b7cc
Mohamed Amine Chafia <chafia.amine1@gmail.com>
2023-10-31 20:34:18 -0400

    Configure Application Startup

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,9 @@
+using EmployeeManager.Mvc.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,10 +15,20 @@ namespace EmployeeManager.Mvc
 {
     public class Startup
     {
+        private IConfiguration config = null;
+
+        public Startup(IConfiguration config) 
+        {
+            this.config = config;
+        }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(this.config.GetConnectionString("AppDb")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +39,13 @@ namespace EmployeeManager.Mvc
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=EmployeeManager}/{action=List}/{id?}");
             });
         }
     }
